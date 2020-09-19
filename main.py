0dfcbcb53f32244b7ef86bf23c4aeb6d9bb0b703
Brandon Guo <bguo@caltech.edu>
2020-09-19 10:37:58 -0700

    adding combined model

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 
 import argparse
+import sys
 
 
+from src.functions import *
 
 
+# Runs all the functions necessary. Returns if any of the intermediate steps fails
+
+def runner():
+    print("### TRAINING THE BASE MODEL###")
+    #naked_model("base_model.h5", "processed_train.pkl", "train_labels.pkl", "processed_test.pkl", "test_labels.pkl")
+    combined_model("base_model.h5", "processed_train.pkl", "train_labels.pkl", "processed_test.pkl", "test_labels.pkl", "race", 0.05)
 # Parse arguments
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='debiasA')
@@ -12,4 +20,6 @@ if __name__ == '__main__':
     parser.add_argument('--test', type = str, help = "The file location (.pkl) of processed test set", default = "processed_test.pkl")
     parser.add_argument('--alpha', type = float, help = "Measure of the adversarial \
     correction for resulting network. Larger 0 < alpha < 1 is more extreme")
-    args = parser.parse_args()
+    #args = parser.parse_args()
+
+    runner()
--- src/functions.py
+++ src/functions.py
@@ -1,9 +1,60 @@
 ## Functions to be imported in main.py
+import tensorflow as tf
 from tensorflow import keras
+import pickle
+import numpy as np
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras import Input, Model
+
+# Helper function to load a pickle object in a more concise way.
+def open_pickle(path):
+    try:
+        with open(path, 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return None
 
 # Run the model naked, i.e. with no corrections. If this fails then there is
 # an issue with the inputs. Also want to see what the bias is like here.
-def naked_model(model_path, train_path, feature):
+
+def naked_model(model_path, train_path, train_labels, test_path, test_labels):
     # Loading the simple model.
     model = keras.models.load_model(model_path)
-    
+    opt = keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(optimizer = opt, loss = "binary_crossentropy", metrics = ['accuracy'])
+
+    train_data = open_pickle(train_path)
+    train_labels = open_pickle(train_labels)
+
+    test_data = open_pickle(test_path)
+    test_labels = open_pickle(test_labels)
+    history = model.fit(train_data, train_labels, batch_size = 32, epochs = 5,\
+     validation_split = 0.2, shuffle = True)
+
+    eval = model.evaluate(test_data, test_labels)
+
+    print("### BASE MODEL EVALUATION")
+    for i in range(len(eval)):
+        print(model.metrics_names[i] + ": " + str(eval[i]))
+
+
+# Run the model with an adversarial component. Need to be given a class (str) that
+# identifies how to run the adversary.
+def combined_model(model_path, train_path, train_labels, test_path, test_label, class_name, alpha):
+    # Need to modify the data based on input for class. Loading in training data.
+    train_data = open_pickle(train_path)
+    if class_name not in train_data.columns:
+        print("Bad class name")
+        return None
+    Y_adv = train_data[class_name]
+    train = train_data.drop([class_name], axis = 1)
+    train_labels = open_pickle(train_labels)
+
+    # Now loading the model
+    model = keras.models.load_model(model_path)
+    output_layer = model.layers[-1].output
+    A1 = Dense(40, activation='relu',name='A1')(output_layer)
+    A2 = Dense(1, activation='sigmoid',name='A2')(A1)
+
+    combo_model = Model(inputs = [model.input], outputs = [output_layer , A2])
+    print(combo_model.summary())
